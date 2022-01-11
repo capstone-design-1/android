f3e65cb75016b26a1d998c861e681ad2680bebda
sladuf <04fud27@gmail.com>
2022-01-11 22:35:09 +0900

    import 삭제

--- app/src/main/java/org/techtown/cpastone_design/MainActivity.java
+++ app/src/main/java/org/techtown/cpastone_design/MainActivity.java
@@ -19,12 +19,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
